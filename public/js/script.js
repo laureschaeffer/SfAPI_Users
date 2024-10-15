bba3499348faac534e8354474a0ab04218eceed2
laureschaeffer <laure.schaeffer@hotmail.com>
2024-10-15 15:33:10 +0200

    debug script

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,14 @@
 const membres_container = document.querySelector("#membres-container");
  
-fetch("http://127.0.0.1:8000/api/membres")
-// fetch("http://localhost:8000/api/membres")
+fetch('http://localhost:8000/api/membres')
   .then((response) => response.json())
-  
   .then((data) => {
-      console.log(response);
-    const membres = data["hydra:member"];
+    const membres = data["member"];
 
-    console.log(membres);
     
-    // membres.forEach((membre) => {
-    //     console.log(membre);
-    //   let membre_box = document.createElement("div");
-    //   membre_box.innerHTML = membre.last.toUpperCase() + " " + membre.first;
-    //   membres_container.appendChild(membre_box);
-    // });
+    membres.forEach((membre) => {
+      let membre_box = document.createElement("div");
+      membre_box.innerHTML = membre.last.toUpperCase() + " " + membre.first;
+      membres_container.appendChild(membre_box);
+    });
   });
